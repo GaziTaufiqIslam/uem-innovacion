6378fa7fb47513afdada1e25c3c02d5acbb220c1
GaziTaufiqIslam <gzidev@gmail.com>
2017-06-10 19:20:11 +0530

    First Commit

---
+++ js/function.js
@@ -0,0 +1,5 @@
+particlesJS.load('particles-js',
+'https://raw.githubusercontent.com/GaziTaufiqIslam/Assests-for-codepen/master/particles.json',
+ function() {
+  console.log('callback - particles.js config loaded');
+});
